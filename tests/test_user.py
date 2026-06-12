f8d6f43d84ac5e9ba1b4f6d89006e386c3e38604
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-06-12 14:32:41 +0300

    user test

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from models.user import User
+
 
 u1 = User("Alex", "alex@email.com")
 u2 = User("John", "john@email.com")
@@ -7,4 +13,4 @@ print(u1)
 print(u2)
 
 u1.add_project("CLI Tool")
-print("Projects for Alex:", u1.get_projects())
+print("Projects:", u1.get_projects())
