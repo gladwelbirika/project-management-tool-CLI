207384848e5d07986836474acb9b16229573954b
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-06-12 14:35:56 +0300

    fix error

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from models.project import Project
+
 
 p1 = Project("CLI Tool", "Build a CLI app")
 
